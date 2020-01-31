74628923e97cbb37969c0973af993daf7e5031a0
THOMAS MARTIN - Student <G00339428@gmit.ie>
2020-01-31 12:56:57 +0000

    Second Commit

--- src/main/java/ie/gmit/Calculator.java
+++ src/main/java/ie/gmit/Calculator.java
@@ -1,7 +1,7 @@
 package ie.gmit;
 
 public class Calculator {
-    public double add(double firstNum, double secondNum){
+    public double add(double firstNum, double secondNum){ //method will add 2 doubles
         return firstNum + secondNum;
     }
 }
